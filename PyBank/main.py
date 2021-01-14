25fd403efc98113e06da2b43f0456259bcd477bc
DPintacasi <73974414+DPintacasi@users.noreply.github.com>
2021-01-13 22:09:14 -0500

    final upload

--- PyBank/main.py
+++ PyBank/main.py
@@ -21,8 +21,9 @@ with open(filepath, 'r') as budget_data_file:
     max_profit_change_index = profit_change.index(max(profit_change))
     min_profit_change_index = profit_change.index(min(profit_change))
 
+print("--------------------------------------------------")
 print("Financial Analysis")
-print("---------------------------------")
+print("--------------------------------------------------")
 print(f"Total Months: {len(months)}")
 print(f"Total Profit: ${sum(profit)}")
 print(f"Average Change: ${round(sum(profit_change)/len(profit_change),2)}")
@@ -30,18 +31,28 @@ print(f"Greatest Increase in Profit: {months[max_profit_change_index]} (${max(pr
 print(f"Greatest Decrease in Profit: {months[min_profit_change_index]} (${min(profit_change)})")
 
 
-filepath_output = os.path.join("analysis","budget_analysis.csv")
+filepath_output = os.path.join("analysis","budget_analysis.txt")
 
-with open(filepath_output, "w", newline = "") as budget_analysis:
+with open(filepath_output, "w", newline = "") as analysis:
 
-    analysis = csv.writer(budget_analysis, delimiter=',')
+    analysis.write("-------------------------------------------------\n")
+    analysis.write("Financial Analysis\n")
+    analysis.write("-------------------------------------------------\n")
+    analysis.write(f"Total Months: {len(months)}\n")
+    analysis.write(f"Total Profit: ${sum(profit)}\n")
+    analysis.write(f"Average Change: ${round(sum(profit_change)/len(profit_change),2)}\n")
+    analysis.write(f"Greatest Increase in Profit: {months[max_profit_change_index]} (${max(profit_change)})\n")
+    analysis.write(f"Greatest Decrease in Profit: {months[min_profit_change_index]} (${min(profit_change)})\n")
 
-    analysis.writerow(["Financial Analysis"])
-    analysis.writerow(["Total Months", len(months)])
-    analysis.writerow(["Total Profit",f"${sum(profit)}"])
-    analysis.writerow(["Average Change",f"${round(sum(profit_change)/len(profit_change),2)}"])
-    analysis.writerow(["Greatest Increase in Profit",months[max_profit_change_index],f"${max(profit_change)}"]) 
-    analysis.writerow(["Greatest Decrease in Profit",months[min_profit_change_index],f"${min(profit_change)}"])
+# with open(filepath_output, "w", newline = "") as budget_analysis:
 
+#     analysis = csv.writer(budget_analysis, delimiter=',')
+
+#     analysis.writerow(["Financial Analysis"])
+#     analysis.writerow(["Total Months", len(months)])
+#     analysis.writerow(["Total Profit",f"${sum(profit)}"])
+#     analysis.writerow(["Average Change",f"${round(sum(profit_change)/len(profit_change),2)}"])
+#     analysis.writerow(["Greatest Increase in Profit",months[max_profit_change_index],f"${max(profit_change)}"]) 
+#     analysis.writerow(["Greatest Decrease in Profit",months[min_profit_change_index],f"${min(profit_change)}"])
 
     
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,8 +1,11 @@
+#import modules
 import os
 import csv
 
+#make file path
 filepath_input = os.path.join("resources","election_data.csv")
 
+#extract data
 with open(filepath_input, 'r') as election_data_file:
 
     data = csv.reader(election_data_file, delimiter = ",")
@@ -25,32 +28,51 @@ with open(filepath_input, 'r') as election_data_file:
         else:
             candidate_vote_count[candidate_list.index(vote)] = candidate_vote_count[candidate_list.index(vote)] + 1
     
+    #zip cadidate and vote count together
+    results = zip(candidate_list,candidate_vote_count)
+
+    #search for winner
     winning_count = max(candidate_vote_count)
     winner = candidate_list[candidate_vote_count.index(winning_count)]
 
- #print results
+#print results
+print("------------------------")
 print("Election Results")
 print("------------------------")
 print(f"Total Votes: {len(votes)}")
 print("------------------------")
-for c in range(len(candidate_list)):
-    print(f"{candidate_list[c]}: {round(candidate_vote_count[c]*100/len(votes),2)}% ({candidate_vote_count[c]})")
+for row in results:
+    print(f"{row[0]}: {round(row[1]*100/len(votes),2)}% ({row[1]})")
 print("------------------------")
 print(f"Winner: {winner}")
 print("------------------------")
 
-filepath_output = os.path.join("analysis","election_analysis.csv")
+#write results to file
+filepath_output = os.path.join("analysis","election_analysis.txt")
 
-with open(filepath_output, "w", newline = "") as election_analysis:
-
-    analysis = csv.writer(election_analysis, delimiter=',')
-
-    analysis.writerow(["Election Results"])
-    analysis.writerow(["Total Votes",len(votes)])
-    analysis.writerow(["candidate","percentage of votes", "no. of votes"])
+with open(filepath_output, "w") as analysis:
+    analysis.write("------------------------\n")
+    analysis.write("Election Results\n")
+    analysis.write("------------------------\n")
+    analysis.write(f"Total Votes: {len(votes)}\n")
+    analysis.write("------------------------\n")
     for c in range(len(candidate_list)):
-        analysis.writerow([candidate_list[c],f"{round(candidate_vote_count[c]*100/len(votes),2)}%",candidate_vote_count[c]])
-    analysis.writerow(["Winner", winner])
+        analysis.write(f"{candidate_list[c]}: {round(candidate_vote_count[c]*100/len(votes),2)}% ({candidate_vote_count[c]})\n")
+    analysis.write("------------------------\n")
+    analysis.write(f"Winner: {winner}\n")
+    analysis.write("------------------------\n")
+
+#for csv file 
+# with open(filepath_output, "w", newline = "") as election_analysis:
+
+#     analysis = csv.writer(election_analysis, delimiter=',')
+
+#     analysis.writerow(["Election Results"])
+#     analysis.writerow(["Total Votes",len(votes)])
+#     analysis.writerow(["candidate","percentage of votes", "no. of votes"])
+#     for c in range(len(candidate_list)):
+#         analysis.writerow([candidate_list[c],f"{round(candidate_vote_count[c]*100/len(votes),2)}%",candidate_vote_count[c]])
+#     analysis.writerow(["Winner", winner])
     
 
 
